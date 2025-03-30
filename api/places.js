d9c1f4cc3e424f810ff481aaf53d12d134b41d2f
vladd111-source <help.implant@gmail.com>
2025-03-30 14:21:45 +0200

    Update places.js

--- api/places.js
+++ api/places.js
@@ -1,7 +1,8 @@
-app.get("/api/places", (req, res) => {
-  res.json([
+export default async function handler(req, res) {
+  const places = [
     { name: "Бранденбургские ворота", city: "Берлин", description: "Один из самых известных символов Берлина." },
     { name: "Английский сад", city: "Мюнхен", description: "Один из крупнейших городских парков в Европе." },
     { name: "Эйфелева башня", city: "Париж", description: "Знаменитая железная башня и символ Парижа." }
-  ]);
-});
+  ];
+  res.status(200).json(places);
+}
