c644481da638d567fcea64b0d8b98d9e6804b397
vladd111-source <help.implant@gmail.com>
2025-04-10 23:17:25 +0200

    Update flights.js

--- api/flights.js
+++ api/flights.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
+   res.setHeader("Access-Control-Allow-Origin", "*"); // или укажи конкретный домен
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // preflight запрос
+  }
   const flights = [
     { from: "Kyiv", to: "Warsaw", date: "2025-04-01", price: 79 },
     { from: "Lviv", to: "Berlin", date: "2025-04-03", price: 99 },
