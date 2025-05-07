009d75430f5a8d196741166b2baec56f920e73e6
vladd111-source <help.implant@gmail.com>
2025-05-07 16:51:09 +0200

    Update proxy-search.js

--- api/proxy-search.js
+++ api/proxy-search.js
@@ -1,6 +1,11 @@
-import fetch from "node-fetch";
-
 const proxySearchHandler = async (req, res) => {
+  if (req.method === "OPTIONS") {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Метод не поддерживается" });
   }
