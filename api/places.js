e55e9ce9fd6b4bbee37551adfa78d4432bbc95e7
vladd111-source <help.implant@gmail.com>
2025-05-19 22:16:14 +0200

    Update places.js

--- api/places.js
+++ api/places.js
@@ -1,3 +1,4 @@
+// ✅ /api/places
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
