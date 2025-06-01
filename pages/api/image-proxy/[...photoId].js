a8fdb21eafe1c389c9e11a69d4acabfef16a4bc1
vladd111-source <help.implant@gmail.com>
2025-06-02 00:11:33 +0200

    Update [...photoId].js

--- pages/api/image-proxy/[...photoId].js
+++ pages/api/image-proxy/[...photoId].js
@@ -13,7 +13,6 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(imageUrl);
-
     if (!response.ok) {
       return res
         .status(response.status)
